991408fff80af1391efb12abcdb12672f1dc53ca
Minas <minasanton@gmail.com>
2018-01-15 21:12:16 +0000

    cache done

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -3,20 +3,22 @@
 dropdown menus and rental listings"""
 from flask import Flask, render_template
 from models import storage
+from uuid import uuid4
 app = Flask('web_flask')
 app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb')
+@app.route('/0-hbnb')
 def display_hbnb():
     """Generate page with popdown menu of states/cities"""
     states = storage.all('State')
     amenities = storage.all('Amenity')
     places = storage.all('Place')
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=states,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id = uuid4())
 
 
 @app.teardown_appcontext
