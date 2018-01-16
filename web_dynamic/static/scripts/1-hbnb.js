b46ad4adc4aca79a5bd58fdb67f56a74fa8cdb3a
Tope <tope628@gmail.com>
2018-01-16 03:11:53 +0000

    script if statement fixed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,11 +2,11 @@ $(() => {
   let aList = [];
   $('li input[type=checkbox]').change(() => {
     let amen = $(this).attr('data-id');
-    if this.checked && $.inArray(amen, aList === -1) {
+    if (this.checked && $.inArray(amen, aList === -1)) {
       aList.push(amen);
-      } else {
-	aList.splice(aList.indexOf(amen), 1);
-      }
+    } else {
+      aList.splice(aList.indexOf(amen), 1);
+    }
     $('.amenities h4').html(aList.length > 0 ? '<em>' + aList.join(', ') + '</em>' : '&nbsp;');
   });
 });
